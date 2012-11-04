6d1a866e17d72f2ec3afc1d7fe27e69345e7607f
Aliaksandr Valialkin <valyala@gmail.com>
2012-11-04 18:58:45 +0200

    fixed libs/go/memcache tests

--- libs/go/memcache/memcache_test.go
+++ libs/go/memcache/memcache_test.go
@@ -21,23 +21,25 @@ func newCache(t *testing.T) *ybc.Cache {
 	return cache
 }
 
-func newServer(t *testing.T) *Server {
-	return &Server{
-		Cache:      newCache(t),
+func newServerCache(t *testing.T) (s *Server, cache *ybc.Cache) {
+	cache = newCache(t)
+	s = &Server{
+		Cache:      cache,
 		ListenAddr: testAddr,
 	}
+	return
 }
 
 func TestServer_StartStop(t *testing.T) {
-	s := newServer(t)
-	defer s.Cache.Close()
+	s, cache := newServerCache(t)
+	defer cache.Close()
 	s.Start()
 	s.Stop()
 }
 
 func TestServer_StartStop_Multi(t *testing.T) {
-	s := newServer(t)
-	defer s.Cache.Close()
+	s, cache := newServerCache(t)
+	defer cache.Close()
 	for i := 0; i < 3; i++ {
 		s.Start()
 		s.Stop()
@@ -45,8 +47,8 @@ func TestServer_StartStop_Multi(t *testing.T) {
 }
 
 func TestServer_Serve(t *testing.T) {
-	s := newServer(t)
-	defer s.Cache.Close()
+	s, cache := newServerCache(t)
+	defer cache.Close()
 	go func() {
 		time.Sleep(time.Millisecond * time.Duration(100))
 		s.Stop()
@@ -55,8 +57,8 @@ func TestServer_Serve(t *testing.T) {
 }
 
 func TestServer_Wait(t *testing.T) {
-	s := newServer(t)
-	defer s.Cache.Close()
+	s, cache := newServerCache(t)
+	defer cache.Close()
 	go func() {
 		time.Sleep(time.Millisecond * time.Duration(100))
 		s.Stop()
@@ -65,27 +67,27 @@ func TestServer_Wait(t *testing.T) {
 	s.Wait()
 }
 
-func newClientServer(t *testing.T) (c *Client, s *Server) {
+func newClientServerCache(t *testing.T) (c *Client, s *Server, cache *ybc.Cache) {
 	c = &Client{
 		ConnectAddr:      testAddr,
 		ReconnectTimeout: time.Millisecond * time.Duration(100),
 	}
-	s = newServer(t)
+	s, cache = newServerCache(t)
 	s.Start()
 	return
 }
 
 func TestClient_StartStop(t *testing.T) {
-	c, s := newClientServer(t)
-	defer s.Cache.Close()
+	c, s, cache := newClientServerCache(t)
+	defer cache.Close()
 	defer s.Stop()
 	c.Start()
 	c.Stop()
 }
 
 func TestClient_StartStop_Multi(t *testing.T) {
-	c, s := newClientServer(t)
-	defer s.Cache.Close()
+	c, s, cache := newClientServerCache(t)
+	defer cache.Close()
 	defer s.Stop()
 	for i := 0; i < 3; i++ {
 		c.Start()
