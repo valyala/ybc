3b66aac9a138cba524d6ab9bfab02ea58b447f1c
Aliaksandr Valialkin <valyala@gmail.com>
2012-11-08 23:08:57 +0200

    Added more comand-line options for memcached app

--- apps/go/memcached/main.go
+++ apps/go/memcached/main.go
@@ -5,13 +5,18 @@ import (
 	"github.com/valyala/ybc/bindings/go/ybc"
 	"github.com/valyala/ybc/libs/go/memcache"
 	"log"
+	"time"
 )
 
 var (
-	listenAddr      = flag.String("listenAddr", ":12221", "TCP address the daemon will listen to")
-	maxItemsCount   = flag.Uint64("maxItemsCount", 1000*1000, "Maximum number of items to store in the cache")
-	cacheSize       = flag.Uint64("cacheSize", 100*1000*1000, "Cache size in bytes")
 	cacheFilePath   = flag.String("cacheFilePath", "", "Path to cache file. Leave empty for anonymous cache")
+	cacheSize       = flag.Uint64("cacheSize", 100*1000*1000, "Cache size in bytes")
+	deHashtableSize = flag.Int("deHashtableSize", 1, "Dogpile effect hashtable size")
+	hotDataSize     = flag.Uint64("hotDataSize", 0, "Hot data size in bytes. 0 disables hot data optimization")
+	hotItemsCount   = flag.Uint64("hotItemsCount", 0, "The number of hot items. 0 disables hot items optimization")
+	listenAddr      = flag.String("listenAddr", ":11211", "TCP address the daemon will listen to")
+	maxItemsCount   = flag.Uint64("maxItemsCount", 1000*1000, "Maximum number of items to store in the cache")
+	syncInterval    = flag.Int("syncInterval", 0, "Sync interval in milliseconds. 0 disables data syncing")
 	readBufferSize  = flag.Int("readBufferSize", 4096, "Buffer size in bytes for incoming requests")
 	writeBufferSize = flag.Int("writeBufferSize", 4096, "Buffer size in bytes for outgoing responses")
 )
@@ -19,12 +24,30 @@ var (
 func main() {
 	flag.Parse()
 
-	config := ybc.NewConfig(ybc.SizeT(*maxItemsCount), ybc.SizeT(*cacheSize))
-	defer config.Close()
+	hotItemsCount_ := ybc.SizeT(*hotItemsCount)
+	if hotItemsCount_ == 0 {
+		hotItemsCount_ = ybc.ConfigDisableHotItems
+	}
+	hotDataSize_ := ybc.SizeT(*hotDataSize)
+	if hotDataSize_ == 0 {
+		hotDataSize_ = ybc.ConfigDisableHotData
+	}
+	syncInterval_ := time.Duration(*syncInterval) * time.Millisecond
+	if syncInterval_ <= 0 {
+		syncInterval_ = ybc.ConfigDisableSync
+	}
+	config := &ybc.Config{
+		MaxItemsCount:   ybc.SizeT(*maxItemsCount),
+		DataFileSize:    ybc.SizeT(*cacheSize),
+		HotItemsCount:   hotItemsCount_,
+		HotDataSize:     hotDataSize_,
+		DeHashtableSize: *deHashtableSize,
+		SyncInterval:    syncInterval_,
+	}
 
 	if *cacheFilePath != "" {
-		config.SetDataFile(*cacheFilePath + ".data")
-		config.SetIndexFile(*cacheFilePath + ".index")
+		config.DataFile = *cacheFilePath + ".data"
+		config.IndexFile = *cacheFilePath + ".index"
 	}
 
 	cache, err := config.OpenCache(true)
