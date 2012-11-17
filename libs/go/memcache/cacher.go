1575bc6f92af7b10ca472b35de48c0e08e6fa26b
Aliaksandr Valialkin <valyala@gmail.com>
2012-11-17 21:10:50 +0200

    Cacher interface refactoring

--- libs/go/memcache/cacher.go
+++ libs/go/memcache/cacher.go
@@ -4,22 +4,32 @@ import (
 	"time"
 )
 
-// Client and DistributedClient implement this interface
-type Cacher interface {
+// Client and DistributedClient implement this interface.
+type Ccacher interface {
+	Cget(item *Citem) error
+	Cset(item *Citem) error
+	CsetNowait(item *Citem)
+}
+
+// Client, DistributedClient and CachingClient implement this interface.
+type Memcacher interface {
 	Start()
 	Stop()
 	Get(item *Item) error
 	GetMulti(items []Item) error
 	GetDe(item *Item, graceDuration time.Duration) error
-	Cget(item *Citem) error
 	Set(item *Item) error
 	SetNowait(item *Item)
-	Cset(item *Citem) error
-	CsetNowait(item *Citem)
 	Delete(key []byte) error
 	DeleteNowait(key []byte)
 	FlushAll() error
 	FlushAllNowait()
 	FlushAllDelayed(expiration time.Duration) error
 	FlushAllDelayedNowait(expiration time.Duration)
+}
+
+// Client and DistributedClient implement this interface.
+type Cacher interface {
+	Memcacher
+	Ccacher
 }
